2d850296e25a658cb8983695d3d54c78ca895520
stalin <info@gmail.com>
2025-06-18 19:50:30 +0530

    mysql script change

--- mysql.sh
+++ mysql.sh
@@ -13,7 +13,7 @@
  N="\e[0m"
 
  CHECK_ROOT(){
-    if [ %USERID -ne 0]
+    if [ %USERID -ne 0 ]
     then 
         echo -e "$R Please run this script with root privileges $N" | tee -a $LOG_FILE
         exit 1
@@ -21,7 +21,7 @@
  }
 
  VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then 
         echo -e "$2 is ... $R FAILED $N" | tee -a $LOG_FILE
         exit 1
